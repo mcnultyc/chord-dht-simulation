08a9b8734491695fd170d1465a8614ea0b41f7bb
Carlos McNulty <cmcnul3@uic.edu>
2019-12-05 18:00:29 -0600

    added finger table code

--- src/main/scala/Driver.scala
+++ src/main/scala/Driver.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration._
 import scala.collection.mutable
 import java.security.MessageDigest
 
-import Server.{FindSuccessor, UpdateTable}
+import Server.{FindSuccessor, TestTable, UpdateTable}
 import akka.NotUsed
 import javax.xml.bind.annotation.adapters.HexBinaryAdapter
 
@@ -26,24 +26,40 @@ object MD5{
   }
 }
 
+// Class to keep metadata for routing requests
+class RouteMetadata{
+  private var hops: Int = 0
+  private var time: Int = 0
+  def updateHops(hops: Int): Unit ={
+    this.hops= hops
+  }
+}
+
+
 object Server{
   sealed trait Command
-
+  // Commands to start and pause the system
   case object Start extends Command
   case object Pause extends Command
 
+  // Commands to update successors and handle routing requests
   final case class GetSuccessor(id: BigInt) extends Command
   final case class SetSuccessor(next: ActorRef[Server.Command], nextId: BigInt) extends Command
   final case class FindSuccessor(replyTo: ActorRef[Server.Command], id: BigInt) extends Command
   final case class FoundSuccessor(successor: ActorRef[Command], id: BigInt) extends Command
-
+  // Commands to update server ids
   final case class SetId(id: BigInt) extends Command
   final case class GetId(replyTo: ActorRef[Command]) extends Command
   final case class RespondId(id: BigInt) extends Command
-
+  // Commands to update table and respond updated table
   case object UpdateTable extends Command
   final case class UpdatedTable(table: List[(BigInt, ActorRef[Command])]) extends Command
+  // Commands to insert and lookup data
+  final case class Insert(data: String) extends Command
+  final case class Lookup(key: BigInt) extends Command
+  final case class FoundData(data: String) extends Command
 
+  case object TestTable extends Command
 
   // Largest value created by a 128 bit hash such as MD5
   val md5Max = BigInt(1) << 128
@@ -63,8 +79,10 @@ class Server(context: ActorContext[Server.Command])
   private var table: List[(BigInt, ActorRef[Command])] = null
   // Set id as hash of context username
   private var id: BigInt = MD5.hash(context.self.toString)
+  // Store data keys in a set
+  private val data = mutable.Set[String]()
   // Ids for finger table entries, n+2^{k-1} for 1 <= k <= 128
-  private var tableIds =
+  private val tableIds =
     (0 to 127).map(i =>{
       var n = this.id + (BigInt(1) << i)
       // Values larger than 2^128 are reduced by 2^128 (quick mod)
@@ -77,8 +95,24 @@ class Server(context: ActorContext[Server.Command])
   /* Find the node in the finger table closest to the ID.
    */
   def closestPrecedingNode(id: BigInt): ActorRef[Command] ={
+    /*
     // TODO check finger table for closest preceding node
-    next // just use next to lookup nodes for now (simple ring)
+    if(table != null){
+      //context.log.info("TABLE IS NON-NULL")
+      // Iterate through finger from largest to smallest key
+      table.reverse.foreach{case (fingerId, ref) =>{
+        // Select node with highest key that can fit the id given
+        if(fingerId >= this.id && fingerId <= id){
+          //context.log.info(s"Closest preceding node: $ref")
+          return ref
+        }
+      }}
+    }
+    else{
+      //context.log.info(s"TABLE IS NULL")
+    }
+    */
+    next // just use next to lookup nodes
   }
 
   def updateTable(parent: ActorRef[Command]): Behavior[NotUsed] ={
@@ -91,7 +125,7 @@ class Server(context: ActorContext[Server.Command])
         val table = mutable.ListBuffer[(BigInt, ActorRef[Command])]()
         Behaviors.receiveMessage{
           case FoundSuccessor(successor, id) => {
-            context.log.info(s"$id: $successor, ${responses+1}")
+            //context.log.info(s"$id: $successor, ${responses+1}")
             responses += 1
             table += ((id, successor))
             // Check if all requests have been responded too
@@ -119,10 +153,12 @@ class Server(context: ActorContext[Server.Command])
           // Check if id greater than largest chord ring or
           // less than the smallest id
           if(id > this.id || id <= nextId){
+            context.log.info(s"Successor found at: $parent")
             replyTo ! FoundSuccessor(next, id)
             Behaviors.stopped
           }
           else{
+            //context.log.info("Calling preceding node")
             // Find the closest preceding node
             val node = closestPrecedingNode(id)
             // Request successor from closest preceding node
@@ -141,18 +177,22 @@ class Server(context: ActorContext[Server.Command])
         else{
           // Check if id falls in range (this.id, id]
           if(id > this.id && id <= nextId){
+            context.log.info(s"Successor found at: $parent")
             // Forward our next to actor that requested successor
             replyTo ! FoundSuccessor(next, id)
             // Stop child session
             Behaviors.stopped
           }
           else{
+            //context.log.info("Calling preceding node")
             // Find the closest preceding node
             val node = closestPrecedingNode(id)
+            //context.log.info(s"Forwarding request to: $node")
             // Request successor from closest preceding node
             node ! FindSuccessor(context.self, id)
             Behaviors.receiveMessage{
               case FoundSuccessor(successor, id) => {
+                //context.log.info(s"Forwarding back to: $replyTo")
                 // Forward successor to actor that requested successor
                 replyTo ! FoundSuccessor(successor, id)
                 // Stop child session
@@ -168,6 +208,7 @@ class Server(context: ActorContext[Server.Command])
   override def onMessage(msg: Command): Behavior[Command] = {
     msg match {
       case FindSuccessor(ref,id) =>
+        //context.log.info(s"Find successor at ${context.self}")
         // Create child session to handle successor request (concurrent)
           context.spawnAnonymous(findSuccessor(context.self, ref, id))
         this
@@ -187,6 +228,10 @@ class Server(context: ActorContext[Server.Command])
         this.table = table
         context.log.info("Updated table has been received!")
         this
+      case TestTable =>
+        val testId = BigInt("333658623028670999750805885164791678408")
+        context.spawnAnonymous(findSuccessor(context.self, context.self, testId))
+        this
       case GetId(replyTo) =>
         replyTo ! RespondId(id)
         this
@@ -201,6 +246,7 @@ class Server(context: ActorContext[Server.Command])
 object ServerManager{
   sealed trait Command
   final case class Add(total: Int) extends Command
+  case object CreateTables extends Command
   case object Shutdown extends Command
 
   def createChordRing(servers: List[(BigInt, ActorRef[Server.Command])]): Unit ={
@@ -217,9 +263,15 @@ object ServerManager{
         prev = ref
       }
     }
-    val last = flatChordRing.last._2
-    last ! UpdateTable
-
+    Thread.sleep(2000)
+    flatChordRing.foreach{
+      case(_, ref) =>{
+      //  ref ! UpdateTable
+      }
+    }
+    flatChordRing.last._2 ! UpdateTable
+    Thread.sleep(500)
+    flatChordRing.last._2 ! TestTable
   }
 
   def apply(): Behavior[Command] = {
@@ -239,6 +291,8 @@ object ServerManager{
             // Create chord ring from server hashes
             createChordRing(servers.toList)
             Behaviors.same
+          case CreateTables =>
+            Behaviors.same
           case Shutdown =>
             Behaviors.stopped
         }
@@ -251,7 +305,6 @@ object Driver extends App {
   // Add 5 servers to the system
   system ! ServerManager.Add(5)
   // Sleep for 7 seconds and then send shutdown signal
-
   Thread.sleep(7000)
   system ! ServerManager.Shutdown
 }
