8c8527413abcc4e503045bf6d404d78f95e80624
Carlos McNulty <cmcnul3@uic.edu>
2019-12-10 14:13:41 -0600

    fixed web client bug

--- src/main/scala/WebClient.scala
+++ src/main/scala/WebClient.scala
@@ -15,25 +15,47 @@ import scala.concurrent.Future
 import scala.util.Random.shuffle
 import scala.io.Source
 import scala.util.{Failure, Success}
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+import akka.stream._
 
+import scala.annotation.tailrec
 
 
 object WebClient {
-
-
-
-
 
   def run(): Unit ={
     implicit val system = ActorSystem()
     implicit val executionContext = system.dispatcher
 
+    def sendGetRequests(files: List[String]): Unit = {
+      // Select a file at random for get request
+      val file = shuffle(files).head
+      val request =
+        HttpRequest(
+          method = HttpMethods.GET,
+          uri = "http://localhost:8080/",
+          entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, file)
+        )
+      val future: Future[HttpResponse] = Http().singleRequest(request)
+      future.onComplete {
+        case Success(response) => {
+          // Unmarshal response entity from http response
+          Unmarshal(response.entity).to[String].onComplete {
+            case Success(data) => {
+              system.log.info(s"HTTP GET [$file]: $data")
+              sendGetRequests(files)
+            }
+            case Failure(exception) => sys.error(exception.getMessage)
+          }
+        }
+        case Failure(exception) => sys.error(exception.getMessage)
+      }
+    }
+    
     system.log.info("STARTING WEB CLIENT")
 
     val input = Source.fromFile("src/main/resources/movies.txt")
@@ -62,36 +84,9 @@ object WebClient {
          case Failure(exception) => sys.error(exception.getMessage)
        }
     })
-
+    
     system.log.info("SENDING GET REQUESTS")
-
-    // Get the list of files from the file
     val files = lines.map(line => line.split("\\|")(0))
-    while(true){
-
-        (1 to 20).foreach( x =>{
-          // Select a file at random for get request
-          val file = shuffle(files).head
-          val request =
-            HttpRequest(
-              method = HttpMethods.GET,
-              uri = "http://localhost:8080/",
-              entity = HttpEntity(ContentTypes.`text/plain(UTF-8)`, file)
-            )
-
-          val future: Future[HttpResponse] = Http().singleRequest(request)
-          future.onComplete{
-            case Success(response) => {
-              // Unmarshal response entity from http response
-              Unmarshal(response.entity).to[String].onComplete{
-                case Success(data) => system.log.info(s"HTTP GET [$file]: $data")
-                case Failure(exception) => sys.error(exception.getMessage)
-              }
-            }
-            case Failure(exception) => sys.error(exception.getMessage)
-          }
-        })// End of foreach
-        Thread.sleep(2000)
-    }
+    sendGetRequests(files)
   }
 }
