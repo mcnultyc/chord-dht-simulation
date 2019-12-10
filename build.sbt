595d44e0f6fa331f21b6a10711f8a48909697d99
Shyam Patel <spate54@uic.edu>
2019-12-09 23:13:40 -0600

    Added preliminary tests

--- build.sbt
+++ build.sbt
@@ -7,12 +7,12 @@ scalaVersion := "2.13.1"
 lazy val akkaVersion = "2.6.0"
 
 libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
   "ch.qos.logback" % "logback-classic" % "1.2.3",
   "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test,
-  "org.scalatest" %% "scalatest" % "3.0.8" % Test,
-  "org.scala-lang.modules" %% "scala-xml" % "1.2.0",
+  "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http" % "10.1.11",
   "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.11",
-  "com.typesafe.akka" %% "akka-http"   % "10.1.11",
-  "com.typesafe.akka" %% "akka-stream" % akkaVersion // or whatever the latest version is
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  "org.scala-lang.modules" %% "scala-xml" % "1.2.0",
+  "org.scalatest" %% "scalatest" % "3.0.8" % Test
 )
---
+++ src/test/scala/WebServerTests.scala
@@ -0,0 +1,43 @@
+/* CS441 Course Project: Chord Algorithm Akka/HTTP-based Simulator
+ * Team:   Carlos Antonio McNulty,  cmcnul3 (Leader)
+ *         Abram Gorgis,            agorgi2
+ *         Priyan Sureshkumar,      psures5
+ *         Shyam Patel,             spate54
+ * Date:   Dec 10, 2019
+ */
+
+import com.typesafe.config.ConfigFactory.load
+import org.scalatest.{FlatSpec, Matchers}
+
+
+// WebServerTests class consisting of unit tests based on the ScalaTest testing framework
+class WebServerTests extends FlatSpec with Matchers {
+  // TESTS 1-10: read from app configuration
+  "Configuration" should "specify akka logger" in {
+    load.getStringList("akka.loggers").get(0) should be ("akka.event.slf4j.Slf4jLogger")
+  }
+  it should "specify akka logging filter" in {
+    load.getString("akka.logging-filter") should be ("akka.event.slf4j.Slf4jLoggingFilter")
+  }
+  it should "specify akka event handlers" in {
+    load.getStringList("akka.event-handlers").get(0) should be ("akka.event.slf4j.Slf4jEventHandler")
+  }
+  it should "specify simulation 1 algorithm" in {
+    load.getString("sim1.alg") should be ("simple")
+  }
+  it should "specify number of servers in simulation 1" in {
+    load.getInt("sim1.num-servers") should be (20)
+  }
+  it should "specify snapshot interval in simulation 1" in {
+    load.getInt("sim1.snapshot-interval") should be (15)
+  }
+  it should "specify simulation 2 algorithm" in {
+    load.getString("sim2.alg") should be ("chord")
+  }
+  it should "specify number of servers in simulation 2" in {
+    load.getInt("sim2.num-servers") should be (20)
+  }
+  it should "specify snapshot interval in simulation 2" in {
+    load.getInt("sim2.snapshot-interval") should be (15)
+  }
+}//end class WebServerTests
